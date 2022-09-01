f2af5183999a5ac5ead09aa09a467f78d7f47362
Sam Freeman <samcfreeman888@gmail.com>
2022-09-01 13:54:26 -0500

    number comparison function works to compare guesses

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
-
+let randomNumber = 0;
+// here is where we generate the first random number
+numberGenerator();
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -23,10 +25,37 @@ app.post('/numbers', (req, res) => {
   // console.log('req.body should be array?!?', req.body);
   numbersGuessed = req.body.guessesObject;
   console.log(numbersGuessed);
-  console.log(typeof numbersGuessed);
-
+  numberComparison(numbersGuessed);
   res.sendStatus(200);
 })
 
 //next work on random number function
+
+function numberGenerator (){
+  randomNumber = Math.floor(Math.random()* (25-1)+1).toString();
+  return randomNumber;
+  
+}
 //create comparison function
+let results = [];
+
+function numberComparison(){
+  let guessArray = Object.values(numbersGuessed);
+  console.log(guessArray);
+  results = []
+  for (let guess of guessArray){
+    if (guess === randomNumber){
+      results.push('win');
+    }
+    else if (guess > randomNumber){
+      results.push('high');
+    }
+    else if (guess < randomNumber){
+      results.push('low');
+    }
+  }
+  console.log(results);
+}
+
+console.log(randomNumber);
+// numberComparison();
